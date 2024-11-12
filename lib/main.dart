80f857dd8581e1d979ab0b6f21a8f0ebe85bcc27
SamahAbdAlrahman <s11941150@stu.najah.edu>
2024-11-12 02:34:08 +0200

    task3_homepage

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,16 @@ debugShowCheckedModeBanner: false,
       home: Scaffold(
         // backgroundColor: Colors.white70,
         appBar: AppBar(
-          backgroundColor: Colors.amber,
-          leading: Icon(Icons.menu , color: Colors.white),
+          backgroundColor: Colors.teal,
+          leading: Icon(Icons.menu , color: Colors.black),
           title: Text('Samah Abdalrahman' , 
-          style :TextStyle(color: Colors.black ,
+          style :TextStyle(color: Colors.white ,
           fontWeight:FontWeight.w700,) ,),
           centerTitle: true,
           actions: [
-                        IconButton(
-              icon: Icon(Icons.person,  color: Colors.white),
-              onPressed: () {},
-            ),
-            Icon(Icons.notifications  , color: Colors.white),
+            Icon(Icons.search  , color: Colors.white),
+
+            Icon(Icons.notification_add  , color: Colors.white),
 
           ],
         ),
@@ -40,20 +38,22 @@ class myBody extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('Hello! I am developer ', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900,color:Colors.amber)),
+          Text('Hello! I am developer ', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900,color:Colors.teal)),
 
           SizedBox(height: 15),
-         Text('Samah Abdalrahman', style: TextStyle(fontSize: 26 , color:Colors.amber ,fontWeight:FontWeight.w700,)),
+         Text('Samah Abdalrahman', style: TextStyle(fontSize: 26 , color:Colors.teal ,fontWeight:FontWeight.w700,)),
+     
          Row(
                   mainAxisAlignment: MainAxisAlignment.center,
 
           children: [
             
-            Icon(Icons.phone, color: Colors.amber,),
+            Icon(Icons.phone, color: Colors.teal,),
             SizedBox(width: 10),
-            Text('0591234567', style: TextStyle(fontSize: 26 , color: Colors.amber,fontWeight:FontWeight.w700 )),
+            Text('0591234567', style: TextStyle(fontSize: 26 , color: Colors.teal,fontWeight:FontWeight.w700 )),
           ],
          ),
+         
         ],
       ),
     );
