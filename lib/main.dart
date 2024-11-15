af0918afea8a9ca0158ace098ac40cd3e7983d8d
SamahAbdAlrahman <s11941150@stu.najah.edu>
2024-11-15 04:59:54 +0200

    task4_login

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,110 @@
-import "package:flutter/material.dart";
-
-void main(){
+import 'package:flutter/material.dart';
+void main() {
   runApp(app());
 }
 class app extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-debugShowCheckedModeBanner: false,
-
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          leading: Icon(Icons.menu , color: Colors.black),
-          title: Text('Samah Abdalrahman' , 
-          style :TextStyle(color: Colors.white ,
-          fontWeight:FontWeight.w700,) ,),
-          centerTitle: true,
-          actions: [
-            
-IconButton(
-  onPressed: (){}, icon:  Icon(Icons.search  , color: Colors.white),
-  ) ,
-  IconButton(
-  onPressed: (){}, icon:  Icon(Icons.notification_add  , color: Colors.white),
-  ) ,
-  
-          ],
-        ),
-        body: myBody(),
-      ),
-    );
+return MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Scaffold(
+   appBar:  AppBar(
+    leading: Icon(Icons.keyboard_arrow_left , color: Colors.white,),
+    backgroundColor: Colors.lightBlue,  
+    title: Text('Login !' , style: TextStyle(color: Colors.white , fontSize: 23,fontWeight:FontWeight.w500),),
+    centerTitle: true,
+    actions: [
+      Icon(Icons.account_circle, color: Colors.white,),
+            SizedBox(width: 15,),
+    ],
+   ),
+  body: MyBody(),
+  )
+);
   }
 }
-class myBody extends StatelessWidget{
+class MyBody extends StatefulWidget{
   @override
+  _MyBodyState createState() => _MyBodyState();
+  
+}
+
+class _MyBodyState extends State <MyBody>{
+String msg='';
+        void login(String email, String password){
+String valid_email='tariq@gmail.com';
+String valid_password='112233';
+
+
+
+       setState(() {
+      if (email.isEmpty || password.isEmpty) {
+        msg ='email and passwoed can’t be empty';
+      } else if (email != valid_email || password != valid_password) {
+        msg ='unvalid email or password';
+      } else {
+        msg = 'welcome to the home page';
+      }
+    });
+  }
+    TextEditingController emailController= new TextEditingController();
+TextEditingController passwoedController= new TextEditingController();
+
+@override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Hello! I am developer ', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900,color:Colors.teal)),
 
-          SizedBox(height: 15),
-         Text('Samah Abdalrahman', style: TextStyle(fontSize: 26 , color:Colors.teal ,fontWeight:FontWeight.w700,)),
-     
-         Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return Center(
+child: Container(
+  width: 300,
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+    // SizedBox(height: 50,),
 
-          children: [
-            
-            Icon(Icons.phone, color: Colors.teal,),
-            SizedBox(width: 10),
-            Text('0591234567', style: TextStyle(fontSize: 26 , color: Colors.teal,fontWeight:FontWeight.w700 )),
-          ],
-         ),
+    TextField(
+               cursorColor:Colors.blue,
 
-        ],
+      controller: emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        labelStyle: TextStyle(color: Colors.lightBlue , fontSize: 20,fontWeight:FontWeight.w500),
+        hintText: 'Write your email ...',
+        hintStyle: TextStyle(color: Colors.redAccent , fontSize:12, fontWeight:FontWeight.w400),
+        border: OutlineInputBorder(),
       ),
-    );
-    
+    ),
+    SizedBox(height: 15,),
+    TextField(
+         cursorColor:Colors.blue,
+      controller: passwoedController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        labelStyle: TextStyle(color: Colors.lightBlue , fontSize: 20,fontWeight:FontWeight.w500),
+        hintText: 'Write your password ...',
+        hintStyle: TextStyle(color: Colors.redAccent , fontSize:12, fontWeight:FontWeight.w400),
+        border: OutlineInputBorder(),
+      ),
+    ),
+        SizedBox(height: 20,),
 
+    ElevatedButton(
+    onPressed: () {
+      login(emailController.text, passwoedController.text);
+       },
+    child: Text('Login',style: TextStyle(color: Colors.lightBlue , fontSize: 20,fontWeight:FontWeight.w500),),),
+    SizedBox(height: 20,),
+    Text(msg,
+    style: TextStyle(
+      color: Colors.redAccent ,
+      fontSize: 18,
+      fontWeight:FontWeight.w500),
+    ),
+ 
+    
+    ],
+  ),
+),    );
   }
 
 }
